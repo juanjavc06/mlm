30da4d15570795afa9144be3563aa50328186b00
adan <adan@ecotasktec.org>
2018-11-15 23:45:50 -0800

    creado controlador balance

---
+++ app/Http/Controllers/BalanceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class BalanceController{
+    
+    function crearMovimiento(Request $request,$id){
+        
+        
+    }
+}
